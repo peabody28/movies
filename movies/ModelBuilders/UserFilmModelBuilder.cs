5c1d663cbd988ae4e6f49c3d28f1405dde2deb45
Maksim Harbacheuski <gorbachewski.m@gmail.com>
2023-02-01 23:56:42 +0300

    /User/Film model change

--- movies/ModelBuilders/UserFilmModelBuilder.cs
+++ movies/ModelBuilders/UserFilmModelBuilder.cs
@@ -5,13 +5,28 @@ namespace movies.ModelBuilders
 {
     public class UserFilmModelBuilder
     {
+        public FilmModelBuilder FilmModelBuilder { get; set; }
+
+        public UserFilmModelBuilder(FilmModelBuilder filmModelBuilder) 
+        {
+            FilmModelBuilder = filmModelBuilder;
+        }
+
         public UserFilmModel Build(IUserFilm userFilm)
         {
+            var filmModel = FilmModelBuilder.Build(userFilm.Film);
+
             return new UserFilmModel
             {
                 Id = userFilm.Id,
-                FilmId = userFilm.Film.Id,
-                SectionName = userFilm.Section?.Name
+                FilmId = filmModel.Id,
+                SectionName = userFilm.Section?.Name,
+                Title = filmModel.Title,
+                Description = filmModel.Description,
+                DirectorName = filmModel.DirectorName,
+                CountryName = filmModel.CountryName,
+                Year = filmModel.Year,
+                Ratings = filmModel.Ratings
             };
         }
     }
