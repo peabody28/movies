ede4f481bce1fcef8862e9c4f1d04e87cbdc1c5c
peabody28 <gorbachewski.m@gmail.com>
2023-02-03 17:27:30 +0300

    Autofac

--- movies/Program.cs
+++ movies/Program.cs
@@ -1,80 +1,36 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using movies;
-using movies.Entities;
-using movies.Interfaces.Entities;
-using movies.Interfaces.Operations;
-using movies.Interfaces.Repositories;
-using movies.ModelBuilders;
-using movies.Operations;
 using movies.Repositories;
-using movies.Validations.Film;
-using movies.Validations.Section;
-using movies.Validations.User;
+using System.Reflection;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthorization();
-builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    var issuer = builder.Configuration.GetSection("AuthOptions:ISSUER").Value;
+    var audience = builder.Configuration.GetSection("AuthOptions:AUDIENCE").Value;
+    var key = builder.Configuration.GetSection("AuthOptions:KEY").Value;
+
+    options.TokenValidationParameters = new TokenValidationParameters
     {
-        var issuer = builder.Configuration.GetSection("AuthOptions:ISSUER").Value;
-        var audience = builder.Configuration.GetSection("AuthOptions:AUDIENCE").Value;
-        var key = builder.Configuration.GetSection("AuthOptions:KEY").Value;
-
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = issuer,
-            ValidateAudience = true,
-            ValidAudience = audience,
-            ValidateLifetime = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!)),
-            ValidateIssuerSigningKey = true,
-        };
-    });
-
-// Add services to the container.
-
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidateAudience = true,
+        ValidAudience = audience,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!)),
+        ValidateIssuerSigningKey = true,
+    };
+});
 
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<FilmDbContext>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IDirectorRepository, DirectorRepository>();
-builder.Services.AddScoped<IRatingRepository, RatingRepository>();
-builder.Services.AddScoped<IRatingTypeRepository, RatingTypeRepository>();
-builder.Services.AddScoped<IFilmRepository, FilmRepository>();
-builder.Services.AddScoped<ICountryRepository, CountryRepository>();
-builder.Services.AddScoped<IUserFilmRepository, UserFilmRepository>();
-builder.Services.AddScoped<ISectionRepository, SectionRepository>();
-
-builder.Services.AddTransient<IUser, UserEntity>();
-builder.Services.AddTransient<ICountry, CountryEntity>();
-builder.Services.AddTransient<IDirector, DirectorEntity>();
-builder.Services.AddTransient<IFilm, FilmEntity>();
-builder.Services.AddTransient<IRating, RatingEntity>();
-builder.Services.AddTransient<IRatingType, RatingTypeEntity>();
-builder.Services.AddTransient<ISection, SectionEntity>();
-builder.Services.AddTransient<IUserFilm, UserFilmEntity>();
-
-builder.Services.AddScoped<IIdentityOperation, IdentityOperation>();
-builder.Services.AddSingleton<IAuthorizationOperation, AuthorizationOperation>();
-builder.Services.AddScoped<IUserOperation, UserOperation>();
-
-builder.Services.AddScoped<FilmValidation, FilmValidation>();
-builder.Services.AddScoped<SectionValidation, SectionValidation>();
-builder.Services.AddScoped<UserValidation, UserValidation>();
-
-builder.Services.AddScoped<FilmModelBuilder, FilmModelBuilder>();
-builder.Services.AddScoped<UserFilmModelBuilder, UserFilmModelBuilder>();
-
-builder.Services.AddSingleton<DependencyFactory, DependencyFactory>();
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 builder.Services.AddCors();
 
@@ -83,6 +39,24 @@ builder.Services.AddFluentValidation(s =>
     s.RegisterValidatorsFromAssemblyContaining<Program>();
     s.ValidatorOptions.CascadeMode = FluentValidation.CascadeMode.StopOnFirstFailure;
 });
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    var assembly = Assembly.GetAssembly(typeof(Program));
+
+    builder.RegisterType<FilmDbContext>().SingleInstance();
+    builder.RegisterType<DependencyFactory>();
+    builder.RegisterAssemblyTypes(assembly).Where(t => t.Name.EndsWith("Repository")).InstancePerLifetimeScope().AsImplementedInterfaces();
+    builder.RegisterAssemblyTypes(assembly).Where(t => t.Name.EndsWith("Builder")).InstancePerLifetimeScope();
+    builder.RegisterAssemblyTypes(assembly).Where(t => t.Name.EndsWith("Operation")).InstancePerLifetimeScope().AsImplementedInterfaces();
+    builder.RegisterAssemblyTypes(assembly).Where(t => t.Name.EndsWith("Validation")).InstancePerLifetimeScope();
+    builder.RegisterAssemblyTypes(assembly).Where(t => t.Name.EndsWith("Validator")).InstancePerLifetimeScope();
+    builder.RegisterAssemblyTypes(assembly).Where(t => t.Name.EndsWith("Entity")).AsImplementedInterfaces();
+});
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
